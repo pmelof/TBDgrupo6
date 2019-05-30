430be9bfc6464d9120241141ee343af73276ad89
daniel <daniel.labra@usach.cl>
2019-05-29 20:48:45 -0400

    cambios y arreglos a script sql

--- Script Sql Creacion de Tablas/creacion_tablas.sql
+++ Script Sql Creacion de Tablas/creacion_tablas.sql
@@ -1,16 +1,19 @@
 /*****************************************/
 /*Drops previos en caso de existir tablas*/
 /*****************************************/
+/*****************************************/
+/*Drops previos en caso de existir tablas*/
+/*****************************************/
+DROP SCHEMA IF EXISTS `bd_series`;
+CREATE SCHEMA IF NOT EXISTS`bd_series`;
+USE `bd_series`;
 DROP TABLE IF EXISTS Tweet;
 DROP TABLE IF EXISTS Palabra_clave;
 DROP TABLE IF EXISTS Temporada;
-DROP TABLE IF EXISTS Episodio;
 DROP TABLE IF EXISTS Actor;
 DROP TABLE IF EXISTS Personaje;
 DROP TABLE IF EXISTS Serie;
-DROP TABLE IF EXISTS Episodio;
 DROP TABLE IF EXISTS Estadistica_tweets;
-DROP TABLE IF EXISTS Emisor;
 
 CREATE TABLE Tweet(
 	ID_tweet INTEGER PRIMARY KEY,
@@ -25,38 +28,24 @@ CREATE TABLE Actor(
   sexo INTEGER
 );
 
-CREATE TABLE Emisor(
-  ID_emisor INTEGER PRIMARY KEY,
-  nombre varchar(64),
-  plataforma INTEGER
-);
-
 CREATE TABLE Serie(
 	ID_serie INTEGER PRIMARY KEY,
-	ID_emisor INTEGER,
+	emisor varchar(64),
+  plataforma INTEGER,
 	nombre varchar(50),
 	fecha_inicio date,
   fecha_fin date,
-  sinopsis varchar(1024),
-  FOREIGN KEY (ID_emisor) REFERENCES Emisor(ID_emisor)
+  sinopsis varchar(1024)
 );
 
 CREATE TABLE Temporada(
 	ID_temporada INTEGER PRIMARY KEY,
   ID_serie INTEGER,
 	numero_temporada int,
+  episodios int,
 	fecha_estreno date,
   FOREIGN KEY (ID_serie) REFERENCES Serie(ID_serie)
 	);
-
-CREATE TABLE Episodio(
-  ID_episodio INTEGER PRIMARY KEY,
-	ID_temporada INTEGER,
-  numero_episodio INTEGER,
-  titulo varchar(32),
-  fecha_estreno date,
-  FOREIGN KEY (ID_temporada) REFERENCES Temporada(ID_temporada)
-);
 
 CREATE TABLE Personaje(
   ID_personaje INTEGER PRIMARY KEY,
@@ -72,12 +61,10 @@ CREATE TABLE Palabra_clave(
 	  ID_palabra INTEGER PRIMARY KEY,
 		ID_actor INTEGER,
 		ID_serie INTEGER,
-		ID_episodio INTEGER,
 		ID_personaje INTEGER,
     palabra varchar(32),
     FOREIGN KEY (ID_actor) REFERENCES Actor(ID_actor),
     FOREIGN KEY (ID_serie) REFERENCES Serie(ID_serie),
-    FOREIGN KEY (ID_episodio) REFERENCES Episodio(ID_episodio),
 		FOREIGN KEY (ID_personaje) REFERENCES Personaje(ID_personaje)
 );
 
@@ -88,7 +75,6 @@ CREATE TABLE Genero(
 CREATE TABLE Estadistica_tweets(
 	ID_estadistica INTEGER PRIMARY KEY,
   ID_serie INTEGER,
-  ID_episodio INTEGER,
   ID_personaje INTEGER,
   ID_actor INTEGER,
 	numero_tweets INTEGER,
@@ -96,7 +82,6 @@ CREATE TABLE Estadistica_tweets(
 	numero_tweets_negativos INTEGER,
 	numero_tweets_neutros INTEGER,
   FOREIGN KEY (ID_serie) REFERENCES Serie(ID_serie),
-  FOREIGN KEY (ID_episodio) REFERENCES Episodio(ID_episodio),
   FOREIGN KEY (ID_personaje) REFERENCES Personaje(ID_personaje),
   FOREIGN KEY (ID_actor) REFERENCES Actor(ID_actor)
 );
@@ -113,6 +98,5 @@ CREATE TABLE Tweet_palabraClave(
 	ID_tweet_palabra INTEGER PRIMARY KEY,
 	ID_tweet INTEGER,
 	ID_palabra INTEGER,
-	FOREIGN KEY (ID_tweet) REFERENCES Tweet(ID_tweet),
-	FOREIGN KEY (ID_palabra) REFERENCES Palabra_clave(ID_palabra)
+	FOREIGN KEY (ID_tweet) REFERENCES Tweet(ID_tweet)
 );
