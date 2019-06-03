01fba71240b566a50781abd6313ceeaef0752735
b-vera <bastian.vera.p@usach.cl>
2019-06-03 18:21:41 -0400

    ADD:Highcharts + cambios de vista

--- frontEnd/SeriesFront/src/views/PercepcionSerie.vue
+++ frontEnd/SeriesFront/src/views/PercepcionSerie.vue
@@ -1,6 +1,55 @@
 <template>
   <el-container>
-    <el-header>Percepción Serie</el-header>
-    <el-main>Principal</el-main>
+    <el-header>
+      <h1>Percepción Serie</h1>
+    </el-header>
+    <el-main>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-card class="box-card">
+            <h3>Filtro</h3>
+          </el-card>
+        </el-col>
+        <el-col :span="18">
+          <el-card class="box-card">
+            <h3>Gráfico</h3>
+            <highcharts :options="chartOptions"></highcharts>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-main>
   </el-container>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            chartOptions: {
+                series: [
+                    {
+                        data: [1, 2, 3], // sample data
+                    },
+                ],
+                chart: {
+                    renderTo: 'container',
+                    type: 'column',
+                },
+            },
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+.el-card {
+    width: 100%;
+    height: 80vh;
+}
+.el-header {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+</style>
--- frontEnd/SeriesFront/src/views/PopularidadPersonaje.vue
+++ frontEnd/SeriesFront/src/views/PopularidadPersonaje.vue
@@ -1,6 +1,54 @@
 <template>
   <el-container>
-    <el-header>Popularidad Personaje</el-header>
-    <el-main>Principal</el-main>
+    <el-header>
+      <h1>Popularidad Personaje</h1>
+    </el-header>
+    <el-main>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <el-card class="box-card">
+            <h3>Filtro</h3>
+          </el-card>
+        </el-col>
+        <el-col :span="18">
+          <el-card class="box-card">
+            <h3>Gráfico</h3>
+            <highcharts :options="chartOptions"></highcharts>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-main>
   </el-container>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            chartOptions: {
+                series: [
+                    {
+                        data: [1, 2, 3], // sample data
+                    },
+                ],
+                chart: {
+                    renderTo: 'container',
+                    type: 'column',
+                },
+            },
+        }
+    },
+}
+</script>
+
+<style scoped>
+.el-card {
+    width: 100%;
+    height: 80vh;
+}
+.el-header {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+</style>
