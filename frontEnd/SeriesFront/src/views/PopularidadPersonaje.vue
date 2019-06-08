3fd5a6a86f7f2ddaddd9fc1cef4c7d295220c6d5
matpower49 <seftongillis@gmail.com>
2019-06-08 14:46:47 -0400

    ADD: Checkboxes y buttons en págs. de gráficos

--- frontEnd/SeriesFront/src/views/PopularidadPersonaje.vue
+++ frontEnd/SeriesFront/src/views/PopularidadPersonaje.vue
@@ -8,7 +8,21 @@
         <el-col :span="6">
           <el-card class="box-card">
             <h3>Filtro</h3>
-            <h5>Seleccione uno o más personajes</h5>
+            <h5>Seleccione uno o<br> más personajes</h5>
+            <label class="container">
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span> Cersei Lannister
+            </label>
+            <label class="container">
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span> Rick Grimes
+            </label>
+            <label class="container">
+                <input type="checkbox" checked="checked">
+                <span class="checkmark"></span> Walter White
+            </label>
+            <br>
+            <label class="button">Filtrar</label>
           </el-card>
         </el-col>
         <el-col :span="18">
@@ -103,5 +117,83 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+}
+
+.container {
+  display: block;
+  position: relative;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 15px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #2196F3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+.button {
+  background-color: #2196F3;
+  border: none;
+  border-radius: 2px;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 15px;
 }
 </style>
